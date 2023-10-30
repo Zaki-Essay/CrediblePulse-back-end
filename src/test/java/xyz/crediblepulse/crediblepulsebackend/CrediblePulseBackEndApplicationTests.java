7384fd1a8c96269ffb3ed0030dead89740f0af6c
Zakariae ESSAIYDY <zakaressaiydy@gmail.com>
2023-10-30 16:03:51 +0100

    first commit

---
+++ src/test/java/xyz/crediblepulse/crediblepulsebackend/CrediblePulseBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.crediblepulse.crediblepulsebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrediblePulseBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
