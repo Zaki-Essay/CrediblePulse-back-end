93b2cddf0786d857767f061244f8783b9237826b
Zakariae ESSAIYDY <zakaressaiydy@gmail.com>
2023-11-06 16:24:51 +0100

    config/CPLUSE-filters_config

--- src/main/java/xyz/crediblepulse/crediblepulsebackend/api/TestController.java
+++ src/main/java/xyz/crediblepulse/crediblepulsebackend/api/TestController.java
@@ -1,12 +1,17 @@
 package xyz.crediblepulse.crediblepulsebackend.api;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import xyz.crediblepulse.crediblepulsebackend.config.security.CurrentUserProvider;
 
 @RestController
+@AllArgsConstructor
 public class TestController {
+
+    CurrentUserProvider currentUserProvider;
     @GetMapping("/test")
     public String test(){
-        return "keyckloack it work";
+        return "hello "+currentUserProvider.getUsername();
     }
 }
