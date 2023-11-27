077a2f7c7456482e18ae9b869df472adaea739c1
Zakariae ESSAIYDY <zakaressaiydy@gmail.com>
2023-11-27 12:59:13 +0100

    fix : add resource forder to test packege

--- src/test/java/xyz/crediblepulse/crediblepulsebackend/CrediblePulseBackEndApplicationTests.java
+++ src/test/java/xyz/crediblepulse/crediblepulsebackend/CrediblePulseBackEndApplicationTests.java
@@ -5,16 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -23,11 +13,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 class CrediblePulseBackEndApplicationTests {
 
-
-
     @Test
     void contextLoads() {
-            Assertions.assertTrue(true);
+        Assertions.assertTrue(true);
     }
 }
-
