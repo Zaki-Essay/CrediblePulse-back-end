e4ee4ab2a4b6e9bfacfa119777e3c867a9365257
Zakariae ESSAIYDY <zakaressaiydy@gmail.com>
2023-11-27 13:07:55 +0100

    fix : add resource forder to test packege

--- src/test/java/xyz/crediblepulse/crediblepulsebackend/CrediblePulseBackEndApplicationTests.java
+++ src/test/java/xyz/crediblepulse/crediblepulsebackend/CrediblePulseBackEndApplicationTests.java
@@ -3,14 +3,11 @@ package xyz.crediblepulse.crediblepulsebackend;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@RunWith(SpringRunner.class)
 class CrediblePulseBackEndApplicationTests {
 
     @Test
